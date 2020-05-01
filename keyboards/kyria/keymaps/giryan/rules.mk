365d2453224426867d2a49eb8d15383d9216048a
Matt Waddilove <mattheww@unity3d.com>
2020-05-01 14:21:20 +0100

    Turn on auto shift

--- keyboards/kyria/keymaps/giryan/rules.mk
+++ keyboards/kyria/keymaps/giryan/rules.mk
@@ -3,3 +3,4 @@ ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 LEADER_ENABLE = yes        # Enable the Leader Key feature
 MOUSEKEY_ENABLE = no
+AUTO_SHIFT_ENABLE = yes
